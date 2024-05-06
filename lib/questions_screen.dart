8bcacf1f91c6bd1c33b89f0e613042a06bc29f24
Hp <adkhamjon.abd@gmail.com>
2024-05-06 22:14:24 +0200

    Change the button stylings

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:adv_basics/answer_button.dart';
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
   @override
@@ -11,6 +11,21 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('data');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The question...', style: TextStyle(
+            color: Colors.white,
+          ),),
+          const SizedBox(height: 30),
+          AnswerButton(answerText: 'Answer 2', onTap: () {}),
+          AnswerButton(answerText: 'Answer 3', onTap: () {}),
+          AnswerButton(answerText: 'Answer 4', onTap: () {})
+
+        ],
+      ),
+    );
   }
 }
